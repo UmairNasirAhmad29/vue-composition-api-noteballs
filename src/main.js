1eaebdb26dee2ebb8a0572fe5a60dd46f990b0fd
UmairNasirAhmad29 <umairnasirahmad@gmail.com>
2025-05-03 23:55:43 +0500

    Renamed the title from .html file, using createRouter using vue-router added new routes code by setting history as createWebHashHistory, created 2 new views i.e, notes and stats, and added routes related to it. Also added RouterView and RouterLinks for navigation for now in the App.vue file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+
+    history: createWebHashHistory(),
+    routes: [ // before creating the routes make sure that you have created the views for those routes
+
+        {
+            path: '/', // specifing by slash so this routes behaves as a root path, but in order to use custom route path we first need to set <RouterView /> in App.vuw file
+            name: 'notes',
+            component: ViewNotes
+        },
+        {
+            path: '/stats',
+            name: 'stats',
+            component: ViewStats
+        }
+
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
