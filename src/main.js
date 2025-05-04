d1c8a8714d1b55695115bf7db9d6ada72c726275
UmairNasirAhmad29 <umairnasirahmad@gmail.com>
2025-05-04 12:06:32 +0500

    removed the code related to the routes from main.js to router/index.js, also moved all the dependencies, created the routes in a seperate route array in index.js file and assigned to the routes index in createRouter function, then at the end exported the default router and imported in main.js file to use it in the use() method

--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
-import ViewNotes from '@/views/ViewNotes.vue'
-import ViewStats from '@/views/ViewStats.vue'
-
-const router = createRouter({
-
-    history: createWebHashHistory(),
-    routes: [ // before creating the routes make sure that you have created the views for those routes
-
-        {
-            path: '/', // specifing by slash so this routes behaves as a root path, but in order to use custom route path we first need to set <RouterView /> in App.vuw file
-            name: 'notes',
-            component: ViewNotes
-        },
-        {
-            path: '/stats',
-            name: 'stats',
-            component: ViewStats
-        }
-
-    ]
-})
+import router from './router'
 
 createApp(App)
     .use(router)
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
+
+const route = [ // before creating the routes make sure that you have created the views for those routes
+
+    {
+        path: '/', // specifing by slash so this routes behaves as a root path, but in order to use custom route path we first need to set <RouterView /> in App.vuw file
+        name: 'notes',
+        component: ViewNotes
+    },
+    {
+        path: '/stats',
+        name: 'stats',
+        component: ViewStats
+    }
+
+]
+
+const router = createRouter({
+
+    history: createWebHashHistory(),
+    routes: route
+})
+
+export default router
