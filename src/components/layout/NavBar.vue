b9153ef174e712a32bdcd36b60a692bf122ddb3c
UmairNasirAhmad29 <umairnasirahmad@gmail.com>
2025-05-04 18:14:02 +0500

    Add few new styles and removed main.css from main.js file

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,26 +1,62 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item is-size-4 is-family-monospace">NoteBalls</div>
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+    <div class="container is-max-dekstop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">NoteBalls</div>
+        <a
+          @click.prevent="showMobileMenu = !showMobileMenu"
+          :class="{ 'is-active': showMobileMenu }"
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-end">
-        <RouterLink to="/" active-class="is-active" class="navbar-item is-size-5 is-family-monospace"> Notes </RouterLink>
-        <RouterLink to="/stats" active-class="is-active" class="navbar-item is-size-5 is-family-monospace"> Stats </RouterLink>
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileMenu }"
+      >
+        <div class="navbar-end">
+          <RouterLink
+            to="/"
+            active-class="is-active"
+            class="navbar-item is-size-5 is-family-monospace"
+          >
+            Notes
+          </RouterLink>
+          <RouterLink
+            to="/stats"
+            active-class="is-active"
+            class="navbar-item is-size-5 is-family-monospace"
+          >
+            Stats
+          </RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+
+<style>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+
+const showMobileMenu = ref(false);
+</script>
